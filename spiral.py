ba0342bc4d818af2e0e81ce8f9e25f894752b37d
xenyal <vinceliii@hotmail.com>
2014-11-17 23:57:47 -0500

    Initial commit for Python script

--- spiral.py
+++ spiral.py
@@ -12,5 +12,39 @@ def spiral(rows, cols):
 			if col < rLim:
 				count += 1
 				col += 1
-				
+				#array - something like [dLim][rLim]
+			else: 
+				dir = 'down'
+				uLim += 1;
+
+		if dir == 'down':
+			if row < dLim:
+				count += 1
+				row += 1
+				#array
+			else:
+				dir = 'left'
+				rLim -= 1
+
+		if dir == 'left':
+			if col >= lLim:
+				counter += 1
+				col -= 1
+				#array
+			else:
+				dir = 'up'
+				dLim += 1
+
+		if dir == 'up':
+			if row >= uLim:
+				count += 1
+				row -= 1
+				#array
+			else:
+				dir = 'right'
+				lLim += 1
+				col -= 1
+
+	print #thearray - which will be [dLim][rLim];
+
 
