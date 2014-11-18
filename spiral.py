f98780945b4ab5829b1b96ec40b59f1fc72e90b0
xenyal <vinceliii@hotmail.com>
2014-11-18 00:08:55 -0500

    Finalized solution

--- spiral.py
+++ spiral.py
@@ -1,50 +1,39 @@
-#!/usr/bin/python
-
 def spiral(rows, cols):
-	row, col, count, uLim, lLim = 0, 0, 0, 0, 0
-	dLim = cols
-	rLim = rows
-	maxCube = dLim * rLim
-	dir = 'right'
-
-	while (count <= maxCube):
-		if dir == 'right':
-			if col < rLim:
-				count += 1
-				col += 1
-				#array - something like [dLim][rLim]
-			else: 
-				dir = 'down'
-				uLim += 1;
-
-		if dir == 'down':
-			if row < dLim:
-				count += 1
-				row += 1
-				#array
-			else:
-				dir = 'left'
-				rLim -= 1
-
-		if dir == 'left':
-			if col >= lLim:
-				counter += 1
-				col -= 1
-				#array
-			else:
-				dir = 'up'
-				dLim += 1
-
-		if dir == 'up':
-			if row >= uLim:
-				count += 1
-				row -= 1
-				#array
-			else:
-				dir = 'right'
-				lLim += 1
-				col -= 1
-
-	print #thearray - which will be [dLim][rLim];
-
+    m = [[0 for i in range(cols)] for j in range (rows)]
+    x = 0
+    y = -1
+    counter = 0
+    col_counter = cols
+    rows_counter = rows - 1
+    while counter <= cols*rows:
+        if counter == cols*rows:
+            return m
+        for i in range (0, col_counter):
+            y+=1
+            counter += 1
+            m[x][y] = counter
+        col_counter -= 1
+        if counter == cols* rows:
+            return m        
+        for i in range (0, rows_counter):
+            x+=1
+            counter += 1
+            m[x][y] = counter
+        rows_counter -= 1      
+        if counter == cols* rows:
+            return m        
+        for i in range (0, col_counter):
+            y-=1
+            counter += 1
+            m[x][y] = counter
+        col_counter -= 1
+        if counter == cols* rows:
+            return m        
+        for i in range (0, rows_counter):
+            x-=1
+            counter += 1
+            m[x][y] = counter
+        rows_counter -= 1      
+    
+print spiral (5,4)    
 
